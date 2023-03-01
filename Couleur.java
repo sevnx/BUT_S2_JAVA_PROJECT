08c92ecccaeed606d99019db66af13ddb0473cb5
Corentin Lenclos <corentin.lenclos@gmail.com>
2023-03-01 16:29:08 +0100

    Add files via upload

---
+++ Couleur.java
@@ -0,0 +1,27 @@
+public class Couleur {
+    public enum ANSI {
+        RESET("\u001B[0m"),
+        BLACK("\u001B[30m"),
+        RED("\u001B[31m"),
+        GREEN("\u001B[32m"),
+        YELLOW("\u001B[33m"),
+        BLUE("\u001B[34m"),
+        PURPLE("\u001B[35m"),
+        CYAN("\u001B[36m"),
+        WHITE("\u001B[37m");
+
+        private final String code;
+
+        ANSI(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+
+    public static void println(ANSI color, String message) {
+        System.out.println(color.getCode() + message + ANSI.RESET.getCode());
+    }
+}
